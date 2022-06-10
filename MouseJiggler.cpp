40131b8d5d819fe2d6fc4fdb8ee303d6b754d280
Yoshi_F <yoshi.android@gmail.com>
2022-06-11 01:17:43 +0900

    リファクタリング

--- MouseJiggler.cpp
+++ MouseJiggler.cpp
@@ -13,20 +13,15 @@ void MouseJiggler::turnOnTheLed(int brightness, int red, int green, int blue) {
 }
 
 void MouseJiggler::turnOnTheStartUp() {
-    MouseJiggler::turnOnTheLed(MouseJiggler::BASE_BRIGHTNESS, 255,
-                               MouseJiggler::NO_COLOR, MouseJiggler::NO_COLOR);
+    turnOnTheLed(BASE_BRIGHTNESS, 255, NO_COLOR, NO_COLOR);
 }
 
 void MouseJiggler::turnOnThePower() {
-    MouseJiggler::turnOnTheLed(MouseJiggler::BASE_BRIGHTNESS,
-                               MouseJiggler::NO_COLOR, MouseJiggler::NO_COLOR,
-                               255);
+    turnOnTheLed(BASE_BRIGHTNESS, NO_COLOR, NO_COLOR, 255);
 }
 
 void MouseJiggler::turnOnTheProc() {
-    MouseJiggler::turnOnTheLed(MouseJiggler::BASE_BRIGHTNESS,
-                               MouseJiggler::NO_COLOR, 255,
-                               MouseJiggler::NO_COLOR);
+    turnOnTheLed(BASE_BRIGHTNESS, NO_COLOR, 255, NO_COLOR);
 }
 
 MouseJiggler::MouseJiggler() {}
@@ -35,18 +30,18 @@ void MouseJiggler::startUp(int rgbLedNumber, int neoPixelPin, int ledPin) {
     NEO_PIXEL =
         Adafruit_NeoPixel(rgbLedNumber, neoPixelPin, NEO_RGB + NEO_KHZ800);
     NEO_PIXEL.begin();
-    MouseJiggler::turnOnTheStartUp();
+    turnOnTheStartUp();
     pinMode(ledPin, OUTPUT);
     delay(1000);
-    MouseJiggler::turnOnThePower();
+    turnOnThePower();
     delay(1000);
     Mouse.begin();
 }
 
 void MouseJiggler::moveJiggle() {
-    MouseJiggler::turnOnTheProc();
-    Mouse.move(MouseJiggler::MOVE_X_AXIS, MouseJiggler::MOVE_Y_AXIS);
+    turnOnTheProc();
+    Mouse.move(MOVE_X_AXIS, MOVE_Y_AXIS);
     delay(200);
-    Mouse.move(MouseJiggler::RETURN_X_AXIS, MouseJiggler::RETURN_Y_AXIS);
-    MouseJiggler::turnOnThePower();
+    Mouse.move(RETURN_X_AXIS, RETURN_Y_AXIS);
+    turnOnThePower();
 }
