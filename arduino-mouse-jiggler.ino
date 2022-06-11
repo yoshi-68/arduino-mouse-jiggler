c07a2dd7b455d8b3b5208dc9d432afc582fc8de0
Yoshi_F <yoshi.android@gmail.com>
2022-06-11 23:39:41 +0900

    staticを削除

--- arduino-mouse-jiggler.ino
+++ arduino-mouse-jiggler.ino
@@ -3,13 +3,13 @@
 #include "MouseJiggler.h"
 
 const MouseJiggler JIGGLER;
-static const int SECOND = 1000;
-static const unsigned long TIME_TO_WAIT_FOR_MOUSE_MOVEMENT =
+const int SECOND = 1000;
+const unsigned long TIME_TO_WAIT_FOR_MOUSE_MOVEMENT =
     (unsigned long)59 * SECOND;
 
-static const int RGB_LED_NUMBER = 1;
-static const int NEOPIXEL_PIN = 4;
-static const int LED_PIN = 13;
+const int RGB_LED_NUMBER = 1;
+const int NEOPIXEL_PIN = 4;
+const int LED_PIN = 13;
 
 void setup() { JIGGLER.startUp(RGB_LED_NUMBER, NEOPIXEL_PIN, LED_PIN); }
 
